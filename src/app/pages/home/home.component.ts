89b25aa3b489f2a73bc64c01c2cc1a1fd05f0db8
Jeki <k.leybros.15@gmail.com>
2024-02-18 14:16:56 +0100

    Récupération des données depuis le service, création des propriétés dans la component et binding dans le html

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { Olympic } from 'src/app/core/models/Olympic';
+import { Participation } from 'src/app/core/models/Participation';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -7,15 +9,49 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+export class HomeComponent implements OnInit, OnDestroy {
+  public loading: boolean = true;
+  public olympics: Olympic[] | null = null;
+  public countriesNumber: number = 0;
+  public josNumbers: number = 0;
+  private destroy$: Subject<boolean> = new Subject();
 
-  public data: any = [{ name: 'test', value: 10 }];
+  public data: any;
   public view: [number, number] = [700, 400];
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
+    this.olympicService
+      .getOlympics()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (olympics: Olympic[] | null) => {
+          if (olympics !== null) {
+            let totalParticipation: Participation[] = [];
+            this.countriesNumber = olympics.length;
+            this.data = olympics.map((o) => {
+              let name: string = o.country;
+              let value: number = 0;
+              o.participations.map((p) => {
+                totalParticipation.push(p);
+                value += p.medalsCount;
+              });
+              return { name, value };
+            });
+            this.josNumbers = totalParticipation
+              .map((x) => x.year)
+              .filter(
+                (value, index, array) => array.indexOf(value) === index
+              ).length;
+          }
+          this.loading = false;
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 }
