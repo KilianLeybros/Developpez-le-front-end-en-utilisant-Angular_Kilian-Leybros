378fd63c3f5050b50e8710718a9caaf7bb356b82
Jeki <k.leybros.15@gmail.com>
2024-02-18 15:59:05 +0100

    Implementation des tests pour details, home component, et OlympicService

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { OlympicService } from './core/services/olympic.service';
 
 describe('AppComponent', () => {
+  let olympicServiceSpy: any;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      providers: [{ provide: OlympicService, useValue: olympicServiceSpy }],
     }).compileComponents();
+    TestBed.inject(OlympicService);
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'olympic-games-starter'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('olympic-games-starter');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('olympic-games-starter app is running!');
   });
 });
--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,16 +1,80 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { OlympicService } from 'src/app/core/services/olympic.service';
+import { of } from 'rxjs';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let olympicServiceSpy: any;
 
   beforeEach(async () => {
+    olympicServiceSpy = jasmine.createSpyObj('OlympicService', ['getOlympics']);
+    olympicServiceSpy.getOlympics.and.returnValue(
+      of([
+        {
+          id: 1,
+          country: 'Italy',
+          participations: [
+            {
+              id: 1,
+              year: 2012,
+              city: 'Londres',
+              medalsCount: 28,
+              athleteCount: 372,
+            },
+            {
+              id: 2,
+              year: 2016,
+              city: 'Rio de Janeiro',
+              medalsCount: 28,
+              athleteCount: 375,
+            },
+            {
+              id: 3,
+              year: 2020,
+              city: 'Tokyo',
+              medalsCount: 40,
+              athleteCount: 381,
+            },
+          ],
+        },
+        {
+          id: 2,
+          country: 'Spain',
+          participations: [
+            {
+              id: 1,
+              year: 2012,
+              city: 'Londres',
+              medalsCount: 20,
+              athleteCount: 315,
+            },
+            {
+              id: 2,
+              year: 2016,
+              city: 'Rio de Janeiro',
+              medalsCount: 17,
+              athleteCount: 312,
+            },
+            {
+              id: 3,
+              year: 2020,
+              city: 'Tokyo',
+              medalsCount: 17,
+              athleteCount: 321,
+            },
+          ],
+        },
+      ])
+    );
+
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      declarations: [HomeComponent],
+      providers: [{ provide: OlympicService, useValue: olympicServiceSpy }],
+    }).compileComponents();
+    TestBed.inject(OlympicService);
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
@@ -19,5 +83,11 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('olympics should be initialized', () => {
+    expect(olympicServiceSpy.getOlympics).toHaveBeenCalled();
+    expect(fixture.componentInstance.josNumbers).toEqual(3);
+    expect(fixture.componentInstance.countriesNumber).toEqual(2);
   });
 });
